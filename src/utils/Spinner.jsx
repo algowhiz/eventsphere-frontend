ddab988c51a9da7cff02cc38bd3abb3a1e70b15b
algowhiz <algowhiz@gmail.com>
2024-10-16 22:26:38 +0530

    spinner css changes

--- src/utils/Spinner.jsx
+++ src/utils/Spinner.jsx
@@ -1,6 +1,8 @@
-// Spinner.js
 const Spinner = () => (
-    <div className="spinner  border-t-2 border-blue-600"></div>
-);
-
-export default Spinner;
+    <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 backdrop-blur-sm z-50">
+      <div className="w-16 h-16 border-4 border-t-4 border-blue-600 rounded-full spinner"></div>
+    </div>
+  );
+  
+  export default Spinner;
+  
